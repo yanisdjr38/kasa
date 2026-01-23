cfb290ee48fb3e54c529e4fbfaa8cc81dae076db
Djouahra Yanis <yanis.djouahra@epitech.eu>
2026-01-23 22:04:18 +0100

    ajout de la police Montserrat et création de la page 404

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -2,9 +2,14 @@ import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <Link to="/">Go to Home</Link>
+    <div className="error__section">
+      <h1 className="code__error">404 </h1>
+      <p className="message__error">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <Link className="Link" to="/">
+        Retourner sur la page d'accueil
+      </Link>
     </div>
   );
 }
