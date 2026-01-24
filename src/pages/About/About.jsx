79213ce645ceeec096be23ab50394039fe87e815
Djouahra Yanis <yanis.djouahra@epitech.eu>
2026-01-24 17:16:18 +0100

    modification des balises <div> et <li> par des balises <details> et <summary> pour une meilleure sémantique et accessibilité. Amélioration du style des éléments <summary> et <p> pour une meilleure présentation. Ajout ed Font Awesome pour l'utilisation d'icônes

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -7,33 +7,33 @@ function About() {
       <Banner bannerImage={aboutBannerImage} bannerText="" />
       <section className="about-section">
         <ul className="about__list">
-          <li>Fiabilité</li>
-          <div>
+          <details>
+            <summary>Fiabilité</summary>
             <p>
               Les annonces postées sur Kasa garantissent une fiabilité totale.
               Les photos sont conformes aux logements, et toutes les
               informations sont régulièrement vérifiées par nos équipes.
             </p>
-          </div>
-          <li>Respect</li>
-          <div>
+          </details>
+          <details>
+            <summary>Respect</summary>
             <p>
               La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
               comportement discriminatoire ou de perturbation du voisinage
               entraînera une exclusion de notre plateforme.
             </p>
-          </div>
-          <li>Service</li>
-          <div>
+          </details>
+          <details>
+            <summary>Service</summary>
             <p>
               La qualité du service est au cœur de notre engagement chez Kasa.
               Nous veillons à ce que chaque interaction, que ce soit avec nos
               hôtes ou nos locataires, soit empreinte de respect et de
               bienveillance.
             </p>
-          </div>
-          <li>Sécurité</li>
-          <div>
+          </details>
+          <details>
+            <summary>Sécurité</summary>
             <p>
               La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
               pour les voyageurs, chaque logement correspond aux critères de
@@ -42,7 +42,7 @@ function About() {
               que les standards sont bien respectés. Nous organisons également
               des ateliers sur la sécurité domestique pour nos hôtes.
             </p>
-          </div>
+          </details>
         </ul>
       </section>
     </>
