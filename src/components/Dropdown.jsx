af17921540e68bfd6f266cabf4e02116b93fe28f
Djouahra Yanis <yanis.djouahra@epitech.eu>
2026-02-11 09:48:35 +0100

    utilisation de useState pour gérer l'état d'ouverture du dropdown ajout d'une animation pour l'ouverture du dropdown ajout de user-select: none pour éviter la sélection du texte lors du clic sur le dropdown

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
+
 function Dropdown({ summary, paragraph }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = (e) => {
+    e.preventDefault();
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <details className="dropdown">
-      <summary>{summary}</summary>
-      <div className="dropdown-content">{paragraph}</div>
+    <details className="dropdown" open={isOpen}>
+      <summary onClick={handleToggle}>{summary}</summary>
+      {isOpen && <div className="dropdown-content">{paragraph}</div>}
     </details>
   );
 }
